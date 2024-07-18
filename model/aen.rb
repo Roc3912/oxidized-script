dac34a4c01bfb5ec763be19e03f6dc3fd99e060e
Roc3912 <98382299+Roc3912@users.noreply.github.com>
2024-07-18 11:03:34 +0800

    Add files via upload

---
+++ model/aen.rb
@@ -0,0 +1,21 @@
+class AEN < Oxidized::Model
+  using Refinements
+
+  # Accedian
+
+  comment '# '
+
+  prompt /^([-\w.\/:?\[\]()]+:\s?)$/
+
+  cmd 'configuration generate-script module all' do |cfg|
+    cfg
+  end
+
+  cmd :all do |cfg|
+    cfg.cut_both
+  end
+
+  cfg :ssh do
+    pre_logout 'exit'
+  end
+end
